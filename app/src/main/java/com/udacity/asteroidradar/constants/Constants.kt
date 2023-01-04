469dd35937364533469cb3634f8f676d52c8ff1e
omar.ezz <omar.ezz@g2k-group.com>
2023-01-04 10:23:38 +0200

    add api_key to BuildConfig

--- app/src/main/java/com/udacity/asteroidradar/constants/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/constants/Constants.kt
@@ -1,8 +1,11 @@
 package com.udacity.asteroidradar.constants
 
+import com.udacity.asteroidradar.BuildConfig
+
+
 object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val api_key = "dY3NPyGSDzgJ5zcWiv7VWOYU8bogiQLBLdKiPOaq"
+    const val api_key  = BuildConfig.api_key
 }
